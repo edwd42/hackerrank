3554ff70f8f30cff973d1f6634d3a6ddcf7bffb5
Edward Dupre <edwd42@gmail.com>
2019-02-01 19:49:35 -0500

    completed challenge solveMeFirst

---
+++ algorithms/solveMeFirst.js
@@ -0,0 +1,19 @@
+
+
+function solveMeFirst(a, b) {
+    // Hint: Type return a+b below  
+    return a+b;
+  }
+  
+  
+  function main() {
+    //   var a = parseInt(readLine());
+    //   var b = parseInt(readLine());
+    var a = 2;
+    var b = 3; 
+
+    var res = solveMeFirst(a, b);
+    console.log(res);
+  }
+  
+  main()
